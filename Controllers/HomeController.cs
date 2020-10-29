6b8140c50db3f22e667a5879869230ade9a8f95f
KP-Bee <apivat.janhom@kingpower.com>
2020-10-29 15:01:42 +0700

    add

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Promotion Page";
+
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            ViewBag.Title = "Create Promotion Page";
+
+            return View();
+        }
+
+        public ActionResult Edit()
+        {
+            ViewBag.Title = "Edit Promotion Page";
+
+            return View();
+        }
+    }
+}
---
+++ Controllers/ShowController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication.Controllers
+{
+    public class ShowController : Controller
+    {
+        // GET: Show
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Index";
+
+            return View();
+        }
+
+        public ActionResult Detail()
+        {
+            ViewBag.Title = "Detail";
+
+            return View();
+        }
+    }
+}
