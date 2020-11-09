c4aecb1d87e2088ef26c11ac7d9c7c62e94247aa
KP-Bee <apivat.janhom@kingpower.com>
2020-11-09 15:10:55 +0700

    backend

---
+++ Service/PromoSerivce.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication.Service.Interface;
+
+namespace WebApplication.Service
+{
+    public class PromoSerivce : IPromo
+    {
+
+    }
+}
